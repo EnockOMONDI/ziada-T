cdde03fc84944acfc7dd4c8da910dbc574f8fd23
Enock <djsean@Enocks-MacBook-Pro.local>
2026-01-28 09:18:21 +0300

    render deploy

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-python -m pip install --upgrade pip
+python3 -m pip install --upgrade pip
 pip install -r requirements.txt
-python manage.py collectstatic --noinput --settings=tours_travels.settings_prod
-python manage.py migrate --settings=tours_travels.settings_prod
-python manage.py createcachetable --settings=tours_travels.settings_prod
+python3 manage.py collectstatic --noinput --settings=tours_travels.settings_prod
+python3 manage.py migrate --settings=tours_travels.settings_prod
+python3 manage.py createcachetable --settings=tours_travels.settings_prod
